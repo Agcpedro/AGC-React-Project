0237729b11e2b64baa2ac4c7ad90e24608ec7045
PEDRO AMANDO GANDOS CITELLI <pedro.citelli@csn.com.br>
2024-09-26 14:55:50 -0300

    Consertando os Erros do Passado

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,10 @@ import Layout from './components/Layout';
 import Lista from './pages/Lista';
 import { MateriasProvider } from './context/MateriaContext';
 import MateriaPage from './pages/MateriaPage';
-import { UserProvider } from './context/UserContext';
-import Login from './pages/Login';
 
 const App: React.FC = () => {
   return (
     <Router>
-      <UserProvider>
       <MateriasProvider>
       <Layout>
       <Routes>
@@ -20,11 +17,9 @@ const App: React.FC = () => {
         <Route path="/usuario" element={<Usuario />} />  {/* Rota para a página de usuários */}
         <Route path="/lista" element={<Lista />} />
         <Route path="/materia/:nome" element={<MateriaPage />} />
-        <Route path="/login" element={<Login/>} />
       </Routes>
       </Layout>
       </MateriasProvider>
-      </UserProvider>
     </Router>
   );
 };
